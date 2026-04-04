7479d6d90a264a4e694ecd94e40e047402e8fe1c
Albouftaim <sayyidalbouftaim@gmai.com>
2026-04-04 19:05:23 +0700

    feat: menambahkan endpoint /test-db untuk di uji coba query

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const db = require('./config/db'); // Memanggil koneksi DB
 const app = express();
 const port = 3000;
 
-// Middleware untuk mem-parsing request body berformat JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Endpoint Utama (GET /)
 app.get('/', (req, res) => {
-  res.json({
-    success: true,
-    message: 'Selamat datang di API AmbaCart - AmbaStackDev!',
+  res.json({ message: 'API AmbaCart Berjalan!' });
+});
+
+// Endpoint Uji Coba Database
+app.get('/test-db', (req, res) => {
+  db.query('SHOW TABLES', (err, results) => {
+    if (err) return res.status(500).json({ error: 'Gagal melakukan query' });
+    res.json({ message: 'Koneksi Sukses!', tables: results });
   });
 });
 
-// Menjalankan server di port yang sudah ditentukan
 app.listen(port, () => {
-  console.log(`Server AmbaCart berjalan di http://localhost:${port}`);
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
