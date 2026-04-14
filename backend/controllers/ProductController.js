01473cfd2208d379c7d05fbf08f1ce77a83b71a7
RakaDr <randikaraka19@gmail.com>
2026-04-14 08:41:40 +0700

    merapikan struktur folder menjadi backend dan frontend

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -2,7 +2,7 @@ const ProductModel = require('../models/ProductModel');
 
 class ProductController {
     static index(req, res) {
-        ProductModel.getAllProducts((err, results) => {
+        ProductModel.getAllProduct((err, results) => {
             if (err) return res.status(500).json({ error: 'Failed to retrieve products' });
             res.json({ message: 'Products retrieved successfully', data: results });
         });
